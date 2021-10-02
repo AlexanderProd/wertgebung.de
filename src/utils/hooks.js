df397eb217b1aa9ea7a326147c0b395c75d5dc27
Alexander Hörl <alexander.horl2@live.de>
2021-10-02 22:22:55 +0200

    added useOnScreen hook

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from 'react'
 
 export const useWindowDimensions = () => {
   const [windowDimensions, setWindowDimensions] = useState(
@@ -6,7 +6,7 @@ export const useWindowDimensions = () => {
   )
 
   function getWindowDimensions() {
-    if (typeof window !== "undefined") {
+    if (typeof window !== 'undefined') {
       const { innerWidth: width, innerHeight: height } = window
       return {
         width,
@@ -25,9 +25,34 @@ export const useWindowDimensions = () => {
       setWindowDimensions(getWindowDimensions())
     }
 
-    window.addEventListener("resize", handleResize)
-    return () => window.removeEventListener("resize", handleResize)
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
   }, [])
 
   return windowDimensions
+}
+
+export const useOnScreen = (ref, rootMargin = '0px') => {
+  // State and setter for storing whether element is visible
+  const [isIntersecting, setIntersecting] = useState(false)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        // Update our state when observer callback fires
+        setIntersecting(entry.isIntersecting)
+      },
+      {
+        rootMargin,
+      }
+    )
+    if (ref.current) {
+      observer.observe(ref.current)
+    }
+    return () => {
+      if (ref.current) observer.unobserve(ref.current)
+    }
+  }, []) // Empty array ensures that effect is only run on mount and unmount
+
+  return isIntersecting
 }
